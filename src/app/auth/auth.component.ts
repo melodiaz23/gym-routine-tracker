4821adf487c6cf15460e9e9ee88f2bf395a1b9a7
melodiaz23 <meli.diaz23@gmail.com>
2024-01-09 20:11:57 -0500

    Update auth component: Submit a form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
 })
 export class AuthComponent {
-
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
 }
