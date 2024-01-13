31610179b0bb5ced5093bf0bb31c2594cac75310
melodiaz23 <meli.diaz23@gmail.com>
2024-01-13 16:09:15 -0500

    chore: update header component HTML and TypeScript file

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  isSmall: boolean = false;
 
+  constructor() {}
+
+  @HostListener('window:resize')
+  onResize(event: Event) {
+    this.isSmall = window.innerWidth < 768 ? true : false;
+  }
+
+  onDisplayMenu() {
+    if (window.innerWidth < 768) {
+      console.log('CLICK');
+    }
+  }
 }
