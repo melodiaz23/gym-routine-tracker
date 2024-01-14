6fd29ef591928e2e95ebede9b36741a45d5f8e82
melodiaz23 <meli.diaz23@gmail.com>
2024-01-14 14:25:45 -0500

    added new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  { path: 'user', component: UserComponent },
+  { path: 'auth', component: AuthComponent },
+];
