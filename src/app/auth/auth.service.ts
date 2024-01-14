1bbc91dfb8263ca7914defddc1e184f09aaf5dfc
melodiaz23 <meli.diaz23@gmail.com>
2024-01-14 12:51:41 -0500

    Implement logic in AuthService

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { EmailValidator } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
-import { User } from './user.model';
+import { Account } from './account.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {}
+export class AuthService {
+  accounts: Account[] = [];
+
+  newUserStatus(username: string, email: string, password: any) {
+    console.log(username, email, password);
+  }
+
+  signUp(username: string, email: string, password: any) {
+    let newAccount = new Account(email, username);
+    this.accounts.push(newAccount);
+    console.log(this.accounts);
+  }
+}
