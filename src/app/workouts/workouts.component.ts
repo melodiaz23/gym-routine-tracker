555d22498a6fd2b73b495558a9a3b136fa854fdc
melodiaz23 <meli.diaz23@gmail.com>
2024-01-15 21:03:20 -0500

    update(workouts): modify workouts component HTML, delete tests, and update component logic

--- src/app/workouts/workouts.component.ts
+++ src/app/workouts/workouts.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { WorkoutItemComponent } from './workout-item/workout-item.component';
+import { Workout } from './workout.model';
+import { WorkoutService } from './workout.service';
 
 @Component({
   selector: 'app-workouts',
   standalone: true,
-  imports: [],
+  imports: [WorkoutItemComponent, CommonModule],
   templateUrl: './workouts.component.html',
-  styleUrl: './workouts.component.css'
+  styleUrl: './workouts.component.css',
 })
 export class WorkoutsComponent {
+  workouts: Workout[] = [];
+  daySelected: number = 0;
 
+  constructor(private workoutService: WorkoutService) {}
+
+  ngOnInit() {
+    this.workouts = this.workoutService.getWorkouts();
+  }
+
+  onDayButton(event: Event, id: number) {
+    this.daySelected = id;
+  }
 }
